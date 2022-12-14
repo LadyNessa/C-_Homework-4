8c7c11cb66f2ed789538127702c8bebf9899f783
Nessa <la-nessa@mail.ru>
2022-12-14 17:19:42 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;
+
+for (int count = 1; count < B; count++)
+{
+    result = result * A;
+}
+Console.Write("Итог: ");
+Console.WriteLine(result);
