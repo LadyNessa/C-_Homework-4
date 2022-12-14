80938111f453b2ca9456d65fc1479b49d3abec71
Nessa <la-nessa@mail.ru>
2022-12-15 00:03:19 +0300

    ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Number = N;
+
+int sum = 0;
+int result = 0;
+while (N > 0)
+{
+    result = N % 10;
+    if (result == 0)
+    {
+        sum = sum + 0;
+    }
+    else if (Number % result == 0)
+    {
+        sum = sum + result;
+    }  
+    N = N / 10;
+}
+Console.Write("Сумма: ");
+Console.WriteLine(sum);
+
